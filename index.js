2240e9b206d020508bef2d23a5c98fc538a674aa
Teng-hao Chang <zhusee2@gmail.com>
2016-12-06 10:30:17 +0800

    Read document and list attachments

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const cheerio = require('cheerio');
 const fs = require('fs');
 
 const DL_DIR = 'downloads';
@@ -11,6 +12,28 @@ const ENDPOINT = 'http://117.56.91.94/KMPublic/searchresult/searchservice.aspx'
   + '&pageindex=0'
   + '&pagesize=10';
 
+const DOC_BASE_URL = 'http://117.56.91.94/KMPublic/';
+
+function readDocumentByKey(docKey, docTitle) {
+  const docUrl = `${DOC_BASE_URL}readdocument.aspx?documentId=${docKey}`
+
+  request({ url: docUrl, jar: true }, (err, response, body) => {
+    console.log(`\n[${docKey}] ${docTitle}`);
+
+    if (response && response.statusCode === 200) {
+      const $ = cheerio.load(body);
+
+      const fileTitleDiv = $('.AttachFileTable td.listmode.zx > div');
+      fileTitleDiv.find('span').remove();
+
+      const fileTitle = fileTitleDiv.text();
+      const fileUrl = $('.AttachFileTable td.listmode.zw a').attr('href');
+
+      console.log(`  附件：${fileTitle}`);
+      console.log(`  ${DOC_BASE_URL}/${fileUrl}\n`);
+    }
+  });
+}
 
 // Get category list
 request({ url: ENDPOINT, jar: true }, (err, response, body) => {
@@ -21,7 +44,7 @@ request({ url: ENDPOINT, jar: true }, (err, response, body) => {
     // Get array of records
     records.forEach((document) => {
       const { UniqueKey, Title } = document;
-      console.log(`[${UniqueKey}] ${Title}`);
+      readDocumentByKey(UniqueKey, Title);
     });
   }
 });
