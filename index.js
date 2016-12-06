bc87116592e6222b024d26e0e00b3e4895078a72
Teng-hao Chang <zhusee2@gmail.com>
2016-12-07 01:48:24 +0800

    Download all files of a Record

--- index.js
+++ index.js
@@ -4,39 +4,6 @@ const fs = require('fs');
 
 const CategoryDownloader = require('./lib/category_downloader');
 
-const DL_DIR = 'downloads';
-
-const ENDPOINT = 'http://117.56.91.94/KMPublic/searchresult/searchservice.aspx'
-  + '?region=documentcategory'
-  + '&searchcategory=207'
-  + '&desc=true'
-  + '&sort=_l_last_modified_datetime'
-  + '&pageindex=0'
-  + '&pagesize=10';
-
-const DOC_BASE_URL = 'http://117.56.91.94/KMPublic/';
-
-function readDocumentByKey(docKey, docTitle) {
-  const docUrl = `${DOC_BASE_URL}readdocument.aspx?documentId=${docKey}`
-
-  request({ url: docUrl, jar: true }, (err, response, body) => {
-    console.log(`\n[${docKey}] ${docTitle}`);
-
-    if (response && response.statusCode === 200) {
-      const $ = cheerio.load(body);
-
-      const fileTitleDiv = $('.AttachFileTable td.listmode.zx > div');
-      fileTitleDiv.find('span').remove();
-
-      const fileTitle = fileTitleDiv.text();
-      const fileUrl = $('.AttachFileTable td.listmode.zw a').attr('href');
-
-      console.log(`  附件：${fileTitle}`);
-      console.log(`  ${DOC_BASE_URL}/${fileUrl}\n`);
-    }
-  });
-}
-
 const downloader = new CategoryDownloader(985);
 downloader.retrieveAllRecords({ pageSize: 100 })
   .then(store => console.log(store.getStore()[0]));
